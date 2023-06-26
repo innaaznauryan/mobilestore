77e252012a85675268bf9f5971a60ad3d124def9
Vahagn <vahag79@gmail.com>
2023-06-26 23:43:10 +0400

    added new folder for images and new js file

---
+++ phones.js
@@ -0,0 +1,99 @@
+const phones = [
+    {
+            id: 1,
+            model: "Galaxy S23 Ultra 256GB (Light Pink)",
+            year: 2023,
+            price: 538900,
+            brand: "Samsung",
+            quantity: 19,
+            image: "S-23-Ultra.jpg"
+    },
+    {
+            id: 2,
+            model: "Galaxy Z Flip 4 5G 128GB (Light Blue)",
+            year: 2022,
+            price: 451900,
+            brand: "Samsung",
+            quantity: 11,
+            image: "Z-Flip-Light Blue.png"
+
+    },
+    {
+        id: 3,
+        model: "Galaxy Z Fold 4 5G 256GB (Beige)",
+        year: 2022,
+        price: 719900,
+        brand: "Samsung",
+        quantity: 9,
+        image: "Z-Fold-(Beige).png"
+
+
+    },
+    {
+        id: 4,
+        model: "Galaxy M14 64GB (Silver)",
+        year: 2023,
+        price: 83900,
+        brand: "Samsung",
+        quantity: 34,
+        image: "M14-(Silver).png"
+    },
+    {
+        id: 5,
+        model: "Galaxy A14 128GB (Light Green)",
+        year: 2023,
+        price: 75900,
+        brand: "Samsung",
+        quantity: 41,
+        image: "A14-(Light Green)"
+    },
+    {
+        id: 6,
+        model: "Galaxy A04s 64GB (Copper)",
+        year: 2022,
+        price: 58900,
+        brand: "Samsung",
+        quantity: 27,
+        image: "A04s-(Copper)"
+    },
+    {
+        id: 7,
+        model: "Galaxy A54 128GB (White)",
+        year: 2023,
+        price: 160900,
+        brand: "Samsung",
+        quantity: 22,
+        image: "A54-(White)"
+
+    },
+    {
+        id: 8,
+        model: "Galaxy S22 Ultra 256GB (Dark Red)",
+        year: 2022,
+        price: 449900,
+        brand: "Samsung",
+        quantity: 8,
+        image: "S22-(Dark Red)"
+
+    },
+    {
+        id: 9,
+        model: "Galaxy S21 FE 5G 128GB (Light Violet)",
+        year: 2022,
+        price: 242900,
+        brand: "Samsung",
+        quantity: 13,
+        image: "S21-(Light Violet)"
+
+    },
+    {
+        id: 10,
+        model: "Galaxy S22+ 256GB (Black)",
+        year: 2022,
+        price: 349900,
+        brand: "Samsung",
+        quantity: 15,
+        image: "S22+(Black)"
+
+    },
+]
